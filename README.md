9c1cea4989187528572e88fc35a88b08298a190b
dudustefanello <eduardostefanello3@gmail.com>
2017-11-23 17:15:56 -0200

    Alterações client.js, início README.md e backup client.js do trello

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# power-du
-olá
+# Guia de uso do [client.js](https://github.com/dudustefanello/power-du/js/client.js)
+
+### getBadges
+Essa variável altera as "etiquetas" fixas de um cartão.
+##### Parâmetros:
+* **title:** Texto acima da badge;
+* **text:** Texto de dentro da badge;
+* **icon:** Ícone .png que só aparece fora do cartão (na lista);
+* **color:** Cores de CSS para dentro da badge;
--- js/client.js
+++ js/client.js
@@ -1,80 +1,4 @@
-/* global TrelloPowerUp */
-
-// we can access Bluebird Promises as follows
 var Promise = TrelloPowerUp.Promise;
-
-/*
-
-Trello Data Access
-
-The following methods show all allowed fields, you only need to include those you want.
-They all return promises that resolve to an object with the requested fields.
-
-Get information about the current board
-t.board('id', 'name', 'url', 'shortLink', 'members')
-
-Get information about the current list (only available when a specific list is in context)
-So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
-t.list('id', 'name', 'cards')
-
-Get information about all open lists on the current board
-t.lists('id', 'name', 'cards')
-
-Get information about the current card (only available when a specific card is in context)
-So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
-t.card('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
-
-Get information about all open cards on the current board
-t.cards('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
-
-Get information about the current active Trello member
-t.member('id', 'fullName', 'username')
-
-For access to the rest of Trello's data, you'll need to use the RESTful API. This will require you to ask the
-user to authorize your Power-Up to access Trello on their behalf. We've included an example of how to
-do this in the `🔑 Authorization Capabilities 🗝` section at the bottom.
-
-*/
-
-/*
-
-Storing/Retrieving Your Own Data
-
-Your Power-Up is afforded 4096 chars of space per scope/visibility
-The following methods return Promises.
-
-Storing data follows the format: t.set('scope', 'visibility', 'key', 'value')
-With the scopes, you can only store data at the 'card' scope when a card is in scope
-So for example in the context of 'card-badges' or 'attachment-sections', but not 'board-badges' or 'show-settings'
-Also keep in mind storing at the 'organization' scope will only work if the active user is a member of the team
-
-Information that is private to the current user, such as tokens should be stored using 'private' at the 'member' scope
-
-t.set('organization', 'private', 'key', 'value');
-t.set('board', 'private', 'key', 'value');
-t.set('card', 'private', 'key', 'value');
-t.set('member', 'private', 'key', 'value');
-
-Information that should be available to all users of the Power-Up should be stored as 'shared'
-
-t.set('organization', 'shared', 'key', 'value');
-t.set('board', 'shared', 'key', 'value');
-t.set('card', 'shared', 'key', 'value');
-t.set('member', 'shared', 'key', 'value');
-
-If you want to set multiple keys at once you can do that like so
-
-t.set('board', 'shared', { key: value, extra: extraValue });
-
-Reading back your data is as simple as
-
-t.get('organization', 'shared', 'key');
-
-Or want all in scope data at once?
-
-t.getAll();
-
-*/
 
 var GLITCH_ICON = './images/glitch.svg';
 var WHITE_ICON = './images/icon-white.svg';
@@ -90,51 +14,15 @@ var getBadges = function(t){
       .then(function(cardName){
             console.log('We just loaded the card name for fun: ' + cardName);
 
-            return [/*{
-                  // dynamic badges can have their function rerun after a set number
-                  // of seconds defined by refresh. Minimum of 10 seconds.
-                  dynamic: function(){
-                  // we could also return a Promise that resolves to this as well if we needed to do something async first
-                  return {
+            return {
+                  // its best to use static badges unless you need your badges to refresh
+                  // you can mix and match between static and dynamic
                   title: 'Detail Badge', // for detail badges only
-                  text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-                  icon: GRAY_ICON, // for card front badges only
-                  color: randomBadgeColor(),
-                  refresh: 10 // in seconds
+                  text: 'Mudei a Cor',
+                  icon: null, // for card front badges only
+                  color: 'red'
             };
-      }
-},*/ {
-      // its best to use static badges unless you need your badges to refresh
-      // you can mix and match between static and dynamic
-      title: 'Detail Badge', // for detail badges only
-      text: 'Mudei a Cor',
-      icon: null, // for card front badges only
-      color: 'red'
-}/*, {
-      // card detail badges (those that appear on the back of cards)
-      // also support callback functions so that you can open for example
-      // open a popup on click
-      title: 'Popup Detail Badge', // for detail badges only
-      text: 'Popup',
-      icon: GRAY_ICON, // for card front badges only
-      callback: function(context) { // function to run on click
-            return context.popup({
-                  title: 'Card Detail Badge Popup',
-                  url: './settings.html',
-                  height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
-            });
-      }
-}, {
-      // or for simpler use cases you can also provide a url
-      // when the user clicks on the card detail badge they will
-      // go to a new tab at that url
-      title: 'URL Detail Badge', // for detail badges only
-      text: 'URL',
-      icon: GRAY_ICON, // for card front badges only
-      url: 'https://trello.com/home',
-      target: 'Trello Landing Page' // optional target for above url
-}*/];
-});
+      });
 };
 
 var boardButtonCallback = function(t){
